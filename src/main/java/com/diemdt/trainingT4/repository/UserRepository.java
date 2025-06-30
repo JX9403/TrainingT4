40a9b5acac72beb5e7cf28c0288216e9fec1d770
Your Name <you@example.com>
2025-06-30 14:08:49 +0700

    add

--- src/main/java/com/diemdt/trainingT4/repository/UserRepository.java
+++ src/main/java/com/diemdt/trainingT4/repository/UserRepository.java
@@ -2,8 +2,6 @@ package com.diemdt.trainingT4.repository;
 
 import com.diemdt.trainingT4.model.User;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
